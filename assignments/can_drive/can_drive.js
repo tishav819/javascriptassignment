28bf625eeced389799fff9ff18331362d3481090
Tisha Verma <tishav819@gmail.com>
2022-03-03 17:06:59 +0530

    Assignment-3 submitted

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,18 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
-    // Write you code here
+    if(hasDrivingLiscence){
+        if(isTired==false &&isSober==true){
+            return "You can drive"
+        }
+        else if(isTired==true){
+            return "You shouldn't drive"
+        }
+        else{
+            return "You shouldn't drive"
+        }
+    }
+    else{
+        return "You can't drive"
+    }
 }
 
 module.exports = CanDrive;
