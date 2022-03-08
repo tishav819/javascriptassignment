6553b15444fc4a87bd73a6b575893dc782f6dcbf
Tisha Verma <tishav819@gmail.com>
2022-03-09 02:12:17 +0530

    Assignment-7submission

---
+++ assignments/todo_list/script.js
@@ -0,0 +1,26 @@
+const Input_todo = document.querySelector('input');
+const add_Button = document.querySelector('button');
+const todos_list = document.querySelector('div.todos');
+
+let count=0;
+
+function add_todo() {
+let input_value = Input_todo.value;
+if (input_value==""){
+    return
+}
+let item=document.createElement('p');
+item.innerHTML=input_value;
+item.setAttribute("key",count);
+todos_list.append(item);
+
+count+=1;
+
+item.addEventListener("click",function () {
+    todos_list.removeChild(item);
+})
+
+Input_todo.value=""
+}
+
+add_Button.addEventListener("click",add_todo)
